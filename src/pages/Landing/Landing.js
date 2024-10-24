07e1f952efe88bef2a17b39cb7e9305fa510f400
unknown <cmduilio@gmail.com>
2024-10-24 14:20:25 -0300

    added text, images and responsive

--- src/pages/Landing/Landing.js
+++ src/pages/Landing/Landing.js
@@ -2,8 +2,10 @@ import React, { useState, useRef } from 'react';
 import background from '../../resources/RoseBackground.jpg';
 import logo from '../../resources/rose-logo.png';
 import bunny from '../../resources/electric-bunny-logo.png';
-import comic from '../../resources/comic.png';
-import comic2 from '../../resources/comic2.png';
+import comic1 from '../../resources/ROSE01.jpg';
+import comic2 from '../../resources/ROSE02.jpg';
+import comic3 from '../../resources/ROSE03.jpg';
+import comic4 from '../../resources/ROSE04.jpg';
 import instaLogo from '../../resources/instagram-logo.png';
 import backmusic from '../../resources/background.wav';
 import { SpeakerWaveIcon, SpeakerXMarkIcon } from '@heroicons/react/24/solid';
@@ -11,12 +13,15 @@ import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/24/solid'
 
 function Landing() {
   const [isVolumeOn, setIsVolumeOn] = useState(false);
+  const [mouseOver, setMouseOver] = useState(false);
   const audioRef = useRef(null);
   const [currentIndex, setCurrentIndex] = useState(0)
 
   const images = [
-    comic,
+    comic1,
     comic2,
+    comic3,
+    comic4,
   ]
 
   const goToPrevious = () => {
@@ -54,16 +59,18 @@ function Landing() {
   return (
     <>
       <audio ref={audioRef} autoPlay loop>
-          <source src={backmusic} type="audio/mpeg" />
-          Your browser does not support the audio element.
+        <source src={backmusic} type="audio/mpeg" />
+        Your browser does not support the audio element.
       </audio>
+      
+      {/* First section */}
       <div className="relative h-screen w-full">
         <div
           className="absolute inset-0 bg-cover bg-center bg-no-repeat"
           style={{ backgroundImage: `url(${background})` }}
         />
-        <div className="relative z-5 flex h-full items-center justify-center">
-          <div className="relative -mt-64">
+        <div className="relative z-10 flex h-full flex-col items-center justify-between p-8">
+          <div className="mt-16 sm:mt-32">
             <a
               href="https://www.instagram.com/electricbunny.studios"
               target="_blank"
@@ -73,41 +80,38 @@ function Landing() {
               <img
                 src={logo}
                 alt="Company Logo"
-                className="h-64 w-64 object-contain"
+                className="h-32 w-32 sm:h-64 sm:w-64 object-contain"
               />
             </a>
           </div>
-        </div>
-        <div className="absolute bottom-8 left-8">
-          <a
-            href="https://www.instagram.com/electricbunny.studios"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="flex items-center"
-          >
-            <img
-              src={bunny}
-              alt="Bottom Left Icon"
-              className="h-32 w-32 object-contain ml-12"
-            />
-          </a>
-        </div>
-        <div className="absolute bottom-8 right-8 flex items-center space-x-2 mr-12" >
-          <span className="text-white text-xs text-right">activá el sonido para una <br/>mejor experiencia</span>
-          <SpeakerWaveIcon className="h-5 w-auto text-white cursor-pointer" style={{display: !isVolumeOn ? "none" : ""}} onClick={() => SpeakerClicked()}/>
-          <SpeakerXMarkIcon className="h-5 w-auto text-white cursor-pointer" style={{display: isVolumeOn ? "none" : ""}} onClick={() => SpeakerClicked()}/>
+          
+          <div className="flex w-full justify-between items-end">
+            <a
+              href="https://www.instagram.com/electricbunny.studios"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="flex items-center"
+            >
+              <img
+                src={bunny}
+                alt="Bottom Left Icon"
+                className="h-16 w-16 sm:h-32 sm:w-32 object-contain"
+              />
+            </a>
+            
+            <div className="flex items-center space-x-2">
+              <span className="text-white text-xs text-right">activá el sonido para una <br/>mejor experiencia</span>
+              <SpeakerWaveIcon className="h-5 w-5 text-white cursor-pointer" style={{display: !isVolumeOn ? "none" : ""}} onClick={SpeakerClicked}/>
+              <SpeakerXMarkIcon className="h-5 w-5 text-white cursor-pointer" style={{display: isVolumeOn ? "none" : ""}} onClick={SpeakerClicked}/>
+            </div>
+          </div>
         </div>
       </div>
 
-      <div className="relative flex h-screen w-full">
-        <div
-          className="absolute inset-0 bg-cover bg-center bg-no-repeat"
-          style={{ backgroundColor: "#091915" }}
-        />
-        
-        <div className="flex w-1/2 flex-col items-start justify-between p-8 ml-16">
-          
-          <div className="relative flex w-full">
+      {/* Second section */}
+      <div className="relative flex flex-col lg:flex-row min-h-screen w-full bg-[#091915]">
+        <div className="flex flex-col justify-between p-8 lg:w-1/2">
+          <div className="mb-8">
             <a
               href="https://www.instagram.com/electricbunny.studios"
               target="_blank"
@@ -117,77 +121,66 @@ function Landing() {
               <img
                 src={instaLogo}
                 alt="Instagram Icon"
-                className="h-8 w-8"
+                className="h-6 w-6 sm:h-8 sm:w-8"
               />
-              <span className="text-white text-3xl text-left ml-2">electricbunny.studios</span>
-            </a>
-          </div>
-          <div>
-            <div className="relative w-full text-white text-left text-lg mb-4">
-              NOMBRE DE LA HISTORIETA
-            </div>
-            <div className="relative w-full text-white text-left text-lg mb-8 max-w-sm">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
-              labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit 
-              esse cillum dolore eu fugiat nulla pariatur. 
-            </div>
-          </div>
-
-          <div className="relative w-full">
-            <a
-              href="https://www.instagram.com/electricbunny.studios"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="block w-56 text-center bg-white text-black py-3 rounded-md font-bold"
-            >
-              IR A NUESTRO PERFIL
+              <span className="text-white text-xl sm:text-3xl ml-2">electricbunny.studios</span>
             </a>
           </div>
           
-
-          <div className="relative flex items-center w-full" >
-            <SpeakerWaveIcon className="h-5 text-white cursor-pointer" style={{display: !isVolumeOn ? "none" : ""}} onClick={() => SpeakerClicked()}/>
-            <SpeakerXMarkIcon className="h-5 text-white cursor-pointer" style={{display: isVolumeOn ? "none" : ""}} onClick={() => SpeakerClicked()}/>
-            <span className="text-white text-xs text-left ml-2">activá el sonido para una <br/>mejor experiencia</span>
+          
+          <div className="text-white text-left text-base sm:text-lg mb-8 max-w-xs">
+            Rose está perdida. No podemos decirles dónde está, quién es o a dónde va.
+            <br/><br/>
+            Rose dejó su hogar atrás. ¿Para qué? Todo a su debido tiempo, ya lo sabrán.
+            <br/><br/>
+            El comienzo de esta historia por ahora es lo único que les podemos mostrar. 
           </div>
 
+          <a
+              href="https://www.instagram.com/electricbunny.studios"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="inline-block w-full sm:w-56 text-center bg-white text-black py-3 rounded-md font-bold mb-8"
+            >
+              IR A NUESTRO PERFIL
+            </a>
+
+          <div className="flex items-center">
+            <SpeakerWaveIcon className="h-5 w-5 text-white cursor-pointer" style={{display: !isVolumeOn ? "none" : ""}} onClick={SpeakerClicked}/>
+            <SpeakerXMarkIcon className="h-5 w-5 text-white cursor-pointer" style={{display: isVolumeOn ? "none" : ""}} onClick={SpeakerClicked}/>
+            <span className="text-white text-xs text-left ml-2">activá el sonido para una <br/>mejor experiencia</span>
+          </div>
         </div>
 
-        <div className="relative flex w-1/2 items-center justify-center">
-        
-        
-          <div className="relative w-full max-w-1/2 max-h-1/2 mr-36">
-            <div className="relative w-full h-full flex justify-center">
+        <div className="flex items-center justify-center p-4 lg:w-1/2 touch-pan-x">
+          <div className="relative w-full max-w-2x1 touch-pan-x"
+                onMouseEnter={()=>setMouseOver(true)}
+                onMouseLeave={()=>setMouseOver(false)}>
+            <div className="relative w-full pb-[80%] touch-pan-x">
               <img
                 src={images[currentIndex]}
                 alt={`Slide ${currentIndex + 1}`}
-                className="h-full object-fit rounded-lg"
+                className="absolute inset-0 w-full h-full object-contain rounded-lg"
               />
             </div>
             <button
               onClick={goToPrevious}
-              className="absolute -left-20 top-1/2 -translate-y-1/2 bg-white/80 text-gray-800 p-2 rounded-full hover:bg-white"
+              className="absolute left-4 top-1/2 -translate-y-1/2 bg-white/80 text-gray-800 p-2 rounded-full hover:bg-white"
               aria-label="Previous image"
+              style={{display: mouseOver? "" : "none"}}
             >
-              <ChevronLeftIcon className="w-8 h-8" />
+              <ChevronLeftIcon className="w-6 h-6" />
             </button>
             <button
               onClick={goToNext}
-              className="absolute -right-20 top-1/2 -translate-y-1/2 bg-white/80 text-gray-800 p-2 rounded-full hover:bg-white"
+              className="absolute right-4 top-1/2 -translate-y-1/2 bg-white/80 text-gray-800 p-2 rounded-full hover:bg-white"
               aria-label="Next image"
+              style={{display: mouseOver? "" : "none"}}
             >
-              <ChevronRightIcon className="w-8 h-8" />
+              <ChevronRightIcon className="w-6 h-6" />
             </button>
           </div>
-          
-          {/*<img
-            src={comic}
-            alt="comic"
-            className="rounded-lg object-cover h-4/5 "
-          />*/}
         </div>
-
       </div>
     </>
   );
